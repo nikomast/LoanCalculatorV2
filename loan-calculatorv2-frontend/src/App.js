3f0e018ff8e7210de7ba4b09926b7d2a5b172354
jakamaton <jakamaton@protonmail.com>
2023-09-07 18:24:08 +0300

    Päivityksiä.

--- loan-calculatorv2-frontend/src/App.js
+++ loan-calculatorv2-frontend/src/App.js
@@ -1,53 +1,112 @@
 import React, { useState } from 'react';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-    const [loans, setLoans] = useState([{}]);
-    const [payment, setPayment] = useState("");
+function LoanCalculator() {
+  const [loans, setLoans] = useState([
+    { owner: '', amount: '', interest: '', minimum_payment: '', cost: '', fine: '' }
+  ]);
 
-    const addLoan = () => {
-        setLoans([...loans, {}]);
-    };
+  const [monthlyPayment, setMonthlyPayment] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(loans, payment);
+  const addLoan = () => {
+    const newLoan = { owner: '', amount: '', interest: '', minimum_payment: '', cost: '', fine: '' };
+    setLoans([...loans, newLoan]);
+  };
 
-        // You can send the loans and payment data to your backend API here...
-    };
+  const removeLoan = (indexToRemove) => {
+    const filteredLoans = loans.filter((_, idx) => idx !== indexToRemove);
+    setLoans(filteredLoans);
+  };
 
-    return (
-        <div className="App">
-            <h2>Loan Calculator</h2>
-            <form onSubmit={handleSubmit}>
-                {loans.map((_, idx) => (
-                    <div key={idx} className="loan-entry">
-                        <input
-                            type="text"
-                            placeholder="Owner e.g., Bank A"
-                            onChange={e => {
-                                const newList = [...loans];
-                                newList[idx].owner = e.target.value;
-                                setLoans(newList);
-                            }}
-                            required
-                        />
-                        {/* Similarly, add other input fields here... */}
-                    </div>
-                ))}
-                <button type="button" onClick={addLoan}>Add Loan</button>
-                <input
-                    type="text"
-                    placeholder="Enter monthly payment"
-                    value={payment}
-                    onChange={e => setPayment(e.target.value)}
-                    required
-                />
-                <button type="submit">Calculate</button>
-            </form>
+  const handleSubmit = () => {
+    // When form is submitted, make an API call (for example, using Axios)
+    axios.post('/api/calculate', { loans, monthlyPayment })
+      .then(response => {
+        // Handle the response here
+      });
+  };
+
+  return (
+    <div>
+      {loans.map((_, idx) => (
+        <div key={idx} className="loan-entry">
+          {/* Your input fields here */}
+          <input
+            type="text"
+            placeholder="Owner"
+            onChange={e => {
+              const newList = [...loans];
+              newList[idx].owner = e.target.value;
+              setLoans(newList);
+            }}
+            required
+          />
+              {<input
+              type="text"
+              placeholder="Amount"
+              onChange={e => {
+                  const newList = [...loans];
+                  newList[idx].owner = e.target.value;
+                  setLoans(newList);
+              }}
+              required
+              />}
+              {<input
+              type="text"
+              placeholder="Interest"
+              onChange={e => {
+                  const newList = [...loans];
+                  newList[idx].owner = e.target.value;
+                  setLoans(newList);
+              }}
+              required
+              />}
+              {<input
+              type="text"
+              placeholder="Minimum payment"
+              onChange={e => {
+                  const newList = [...loans];
+                  newList[idx].owner = e.target.value;
+                  setLoans(newList);
+              }}
+              required
+              />}
+              {<input
+              type="text"
+              placeholder="Cost"
+              onChange={e => {
+                const newList = [...loans];
+                newList[idx].owner = e.target.value;
+                setLoans(newList);
+              }}
+              required
+              />}
+              {<input
+              type="text"
+              placeholder="Fine"
+              onChange={e => {
+                const newList = [...loans];
+                newList[idx].owner = e.target.value;
+                setLoans(newList);
+              }}
+              required
+              />}
+          <button onClick={addLoan}>Add Loan</button>
+          <button onClick={() => removeLoan(idx)}>Remove</button>
         </div>
-    );
+      ))}
+        <div>
+        <label>Monthly loan amortization budget: </label>
+        <input
+          type="number"
+          placeholder=""
+          value={monthlyPayment}
+          onChange={e => setMonthlyPayment(e.target.value)}
+        />
+      <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
+  );
 }
 
-export default App;
-
+export default LoanCalculator;
