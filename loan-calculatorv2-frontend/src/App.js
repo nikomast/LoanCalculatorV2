402f8a6ec588babc82be8538669dda8e6552fe8a
jakamaton <jakamaton@protonmail.com>
2023-09-07 14:20:39 +0300

    Toiminnot alkaa muodostua

--- loan-calculatorv2-frontend/src/App.js
+++ loan-calculatorv2-frontend/src/App.js
@@ -1,28 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-    const [message, setMessage] = useState("");
+    const [loans, setLoans] = useState([{}]);
+    const [payment, setPayment] = useState("");
 
-    useEffect(() => {
-      const fetchMessage = async () => {
-        try {
-            const response = await axios.get('http://localhost:5000/hello');
-            setMessage(response.data);
-            console.log("Set message:", response.data);
-        } catch (error) {
-            console.error("Error fetching message:", error);
-        }
-    }; 
-        fetchMessage();
-    }, []);
+    const addLoan = () => {
+        setLoans([...loans, {}]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(loans, payment);
+
+        // You can send the loans and payment data to your backend API here...
+    };
 
     return (
-      <div className="App" key={message}>
-          <h1>{message}</h1>
-      </div>
-  );
-  
+        <div className="App">
+            <h2>Loan Calculator</h2>
+            <form onSubmit={handleSubmit}>
+                {loans.map((_, idx) => (
+                    <div key={idx} className="loan-entry">
+                        <input
+                            type="text"
+                            placeholder="Owner e.g., Bank A"
+                            onChange={e => {
+                                const newList = [...loans];
+                                newList[idx].owner = e.target.value;
+                                setLoans(newList);
+                            }}
+                            required
+                        />
+                        {/* Similarly, add other input fields here... */}
+                    </div>
+                ))}
+                <button type="button" onClick={addLoan}>Add Loan</button>
+                <input
+                    type="text"
+                    placeholder="Enter monthly payment"
+                    value={payment}
+                    onChange={e => setPayment(e.target.value)}
+                    required
+                />
+                <button type="submit">Calculate</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
